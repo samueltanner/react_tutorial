9fd5f028e04224f8f293fa03c5d8b7163d124361
samueltanner <samuel.m.s.tanner@gmail.com>
2021-05-14 15:03:50 -0700

    makes basic react app

---
+++ src/Tess.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class Tess extends Component {
+  render () {
+    return (
+      <div className="ass">
+        <p>Ass</p>
+        <p>Ass</p>
+        <p>Ass</p>
+      </div>
+    )
+  }
+}
+export default Tess
